784333133ed2d5d8fc397c81a6cc27b04ccfb11e
Mark Murnane <mark@hackafe.net>
2018-02-18 14:56:57 -0500

    Pulling universe when loading map

--- frontend/src/components/universe/Play.vue
+++ frontend/src/components/universe/Play.vue
@@ -7,7 +7,7 @@
                 </a-assets>
                 <a-entity id="camera" look-controls wasd-controls ref="camera" position="0 1 0" camera></a-entity>
                 <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
-                <a-text position="0 2.5 -3" align="center" color="#000000" scale="2 2 2" :value="$route.params.name"></a-text>
+                <a-text position="0 2.5 -3" align="center" color="#000000" scale="2 2 2" :value="universe.name"></a-text>
                 <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
                 <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
                 <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
@@ -19,5 +19,16 @@
 
 <script>
 export default {
+    data() {
+        return {
+            universe: null
+        }
+    },
+    mounted() {
+        var self = this;
+        this.$wamp.call('universe.get', [$route.params.id]).then(function(res) {
+            self.universe = res;
+        });
+    }
 }
 </script>
--- frontend/src/routers/index.js
+++ frontend/src/routers/index.js
@@ -41,7 +41,7 @@ export default new Router({
             ]
         },
         {
-            path: '/universe/:name',
+            path: '/universe/:id',
             component: GameLayout,
             children: [
                 {
