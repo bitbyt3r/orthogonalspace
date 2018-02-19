30824fcecb8d9e191467371220bc136156e431f5
Mark Murnane <mark@hackafe.net>
2018-02-18 23:32:31 -0500

    Working on ship builder

--- frontend/src/components/faction/Faction.vue
+++ frontend/src/components/faction/Faction.vue
@@ -1,38 +1,69 @@
 <template>
     <div>
         <md-progress v-if="!loaded" style="margin-top: 200px" md-indeterminate></md-progress>
-        <md-table-card v-else>
-            <md-toolbar>
-                <h1 class="md-title">Ships</h1>
-                <md-button class="md-icon-button">
-                    <md-icon>search</md-icon>
-                </md-button>
-            </md-toolbar>
-            <md-table md-sort='name'>
-                <md-table-header>
-                    <md-table-row>
-                        <md-table-head md-sort-by='name'>Name</md-table-head>
-                        <md-table-head md-sort-by='guns'>Guns</md-table-head>
-                        <md-table-head md-sort-by='books'>Books</md-table-head>
-                    </md-table-row>
-                </md-table-header>
-                <md-table-body>
-                    <md-table-row v-for="ship,id in ships" :key="id">
-                        <md-table-cell>
-                            <router-link :to="'/ship/' + id">
-                                <h3>{{ ship.name }}</h3>
-                            </router-link>
-                        </md-table-cell>
-                        <md-table-cell>
-                            <p>many</p>
-                        </md-table-cell>
-                        <md-table-cell>
-                            <p>few</p>
-                        </md-table-cell>
-                    </md-table-row>
-                </md-table-body>
-            </md-table>
-        </md-table-card>
+        <div>
+            <md-table-card>
+                <md-toolbar>
+                    <h1 class="md-title">Preparing to Launch</h1>
+                    <md-button class="md-icon-button">
+                        <md-icon>search</md-icon>
+                    </md-button>
+                </md-toolbar>
+                <md-table md-sort='name'>
+                    <md-table-header>
+                        <md-table-row>
+                            <md-table-head md-sort-by='name'>Name</md-table-head>
+                            <md-table-head md-sort-by='players'>Players</md-table-head>
+                        </md-table-row>
+                    </md-table-header>
+                    <md-table-body>
+                        <md-table-row v-for="ship,id in ships" v-if="!ship.launched" :key="id">
+                            <md-table-cell>
+                                <router-link :to="'/ship/' + id">
+                                    <h3>{{ ship.name }}</h3>
+                                </router-link>
+                            </md-table-cell>
+                            <md-table-cell>
+                                <p>5</p>
+                            </md-table-cell>
+                        </md-table-row>
+                    </md-table-body>
+                </md-table>
+            </md-table-card>
+            <br>
+            <md-table-card>
+                <md-toolbar>
+                    <h1 class="md-title">Underway</h1>
+                    <md-button class="md-icon-button">
+                        <md-icon>search</md-icon>
+                    </md-button>
+                </md-toolbar>
+                <md-table md-sort='name'>
+                    <md-table-header>
+                        <md-table-row>
+                            <md-table-head md-sort-by='name'>Name</md-table-head>
+                            <md-table-head md-sort-by='guns'>Guns</md-table-head>
+                            <md-table-head md-sort-by='books'>Books</md-table-head>
+                        </md-table-row>
+                    </md-table-header>
+                    <md-table-body>
+                        <md-table-row v-for="ship,id in ships" v-if="ship.launched" :key="id">
+                            <md-table-cell>
+                                <router-link :to="'/ship/' + id">
+                                    <h3>{{ ship.name }}</h3>
+                                </router-link>
+                            </md-table-cell>
+                            <md-table-cell>
+                                <p>many</p>
+                            </md-table-cell>
+                            <md-table-cell>
+                                <p>few</p>
+                            </md-table-cell>
+                        </md-table-row>
+                    </md-table-body>
+                </md-table>
+            </md-table-card>
+        </div>
         <router-link tag="md-button" :to="'/faction/' + $route.params.factionid + '/ship/create'" class="md-fab md-fab-bottom-right">
             <md-icon>add</md-icon>
         </router-link>
--- frontend/src/components/ship/Ship.vue
+++ frontend/src/components/ship/Ship.vue
@@ -1,7 +1,48 @@
 <template>
     <div>
-        <h1>Look at this cool ship:</h1>
-        {{ ship.name }}
+        <div v-if="ship.launched">
+            {{ ship.name }} is underway. Click here to join the crew:
+            <md-button class="md-primary md-raised" :disabled="false" @click="enlist">Enlist</md-button>
+        </div>
+        <div v-else>
+            <h1>Customize Your Ship</h1>
+            <md-layout md-row>
+                <md-layout md-flex-small="100">
+                    <form>
+                        <md-input-container required="true" :class="{'md-input-invalid': errors.has('name')}">
+                            <label>Name</label>
+                            <md-input name="name" v-model="ship.name" v-validate="'required'"></md-input>
+                            <span v-show="errors.has('name')">{{ errors.first('name') }}</span>
+                        </md-input-container>
+                        <md-input-container>
+                            <label for="shiptype">Ship Type</label>
+                            <md-select name="shiptype" id="shiptype" v-model="ship.type">
+                              <md-option v-for="(type, id) in shiptypes" :key="id" :value="id">{{ type.name }}</md-option>
+                            </md-select>
+                        </md-input-container>
+                        <md-input-container>
+                            <md-checkbox id="ready" name="ready" v-model="ready">Ready</md-checkbox>
+                        </md-input-container>
+                    </form>
+                </md-layout>
+                <md-layout md-flex-small="100" md-hide-xsmall>
+                    <div style="height: 300px; width: 300px">
+                        <a-scene debug embedded ref="scene">
+                            <a-assets>
+                            </a-assets>
+                            <a-entity id="camera" look-controls wasd-controls ref="camera" position="0 1 0" camera></a-entity>
+                            <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
+                            <a-text position="0 2.5 -3" align="center" color="#000000" scale="2 2 2" :value="ship.name"></a-text>
+                            <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
+                            <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
+                            <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
+                            <a-sky color="#ECECEC"></a-sky>
+                        </a-scene>
+                    </div>
+                    <p>{{ description }}</p>
+                </md-layout>
+            </md-layout>
+        </div>
     </div>
 </template>
 
@@ -11,7 +52,17 @@ export default {
         return {
             ship: {
                 name: "UNKNOWN Ship"
+            },
+            ready: false,
+            shiptypes: {}
+        }
+    },
+    computed: {
+        description() {
+            if (this.ship.type) {
+                return this.shiptypes[this.ship.type].description;
             }
+            return "Select a ship type to see a description";
         }
     },
     mounted() {
@@ -21,6 +72,11 @@ export default {
                 self.ship = res.ship;
             } else {
                 self.notify("Could not load ship: " + res.reason);
+            }
+        });
+        this.$wamp.call('ship.list_types').then(function(res) {
+            if (res.success) {
+                self.shiptypes = res.types;
             }
         });
     }
