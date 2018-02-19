7ed8df731d0cfde14c9ea200c7f990466b9c9105
Mark Murnane <mark@hackafe.net>
2018-02-18 23:44:06 -0500

    Trying to fix viewport

--- frontend/src/components/ship/Ship.vue
+++ frontend/src/components/ship/Ship.vue
@@ -20,31 +20,34 @@
                               <md-option v-for="(type, id) in shiptypes" :key="id" :value="id">{{ type.name }}</md-option>
                             </md-select>
                         </md-input-container>
-                        <md-input-container>
-                            <md-checkbox id="ready" name="ready" v-model="ready">Ready</md-checkbox>
-                        </md-input-container>
+                        <md-checkbox id="ready" name="ready" v-model="ready">Ready</md-checkbox>
                     </form>
                 </md-layout>
                 <md-layout md-flex-small="100" md-hide-xsmall>
-                    <div style="height: 300px; width: 300px">
-                        <a-scene debug embedded ref="scene">
-                            <a-assets>
-                            </a-assets>
-                            <a-entity id="camera" look-controls wasd-controls ref="camera" position="0 1 0" camera></a-entity>
-                            <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
-                            <a-text position="0 2.5 -3" align="center" color="#000000" scale="2 2 2" :value="ship.name"></a-text>
-                            <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
-                            <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
-                            <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
-                            <a-sky color="#ECECEC"></a-sky>
+                    <a-scene debug embedded>
+                        <a-assets>
+                        </a-assets>
+                        <a-entity id="camera" look-controls wasd-controls ref="camera" position="0 1 0" camera></a-entity>
+                        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
+                        <a-text position="0 2.5 -3" align="center" color="#000000" scale="2 2 2" :value="ship.name"></a-text>
+                        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
+                        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
+                        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
+                        <a-sky color="#ECECEC"></a-sky>
                         </a-scene>
-                    </div>
                     <p>{{ description }}</p>
                 </md-layout>
             </md-layout>
         </div>
     </div>
 </template>
+
+<style>
+a-scene {
+  height: 300px;
+  width: 600px;
+}
+</style>
 
 <script>
 export default {
